1d879364f6b4983373ea66382d5a25b7e2c38ca2
Darianchic1 <115414219+Darianchic1@users.noreply.github.com>
2024-04-25 19:06:38 +0300

    feat: les40 ready

--- Course/Lesson40/final/Delivery/Program.cs
+++ Course/Lesson40/final/Delivery/Program.cs
@@ -11,26 +11,26 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
  
-builder.Services.AddSingleton<ITaskManager>(provider =>
+builder.Services.AddSingleton<IOrderManager>(provider =>
 {
-    var optionsBuilder = new DbContextOptionsBuilder<TaskTrackerContext>();
+    var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
     optionsBuilder.UseSqlite("Data Source=TaskDataBase.db"); 
-    var taskContext = new TaskTrackerContext(optionsBuilder.Options);
-    taskContext.Database.EnsureCreated(); 
-    ITaskRepository taskRepository = new TaskRepository(taskContext);
-    ITaskManager taskManager = new TaskManager(taskRepository);
+    var orderContext = new OrderContext(optionsBuilder.Options);
+    orderContext.Database.EnsureCreated(); 
+    IOrderRepository orderRepository = new OrderRepository(orderContext);
+    IOrderManager orderManager = new OrderManager(orderRepository);
 
-    return taskManager;
+    return orderManager;
 });
 
 builder.Services.AddSingleton<IAccountManager>(provider =>
 {
-    var optionsBuilder = new DbContextOptionsBuilder<AccountContext>();
+    var optionsBuilder = new DbContextOptionsBuilder<UserContext>();
     optionsBuilder.UseSqlite("Data Source=AccountDataBase.db");
-    var accountContext = new AccountContext(optionsBuilder.Options);
-    accountContext.Database.EnsureCreated();
-    IAccountManager accountManager = new AccountManager(accountContext);
-    return accountManager;
+    var accountContext = new UserContext(optionsBuilder.Options);
+    userContext.Database.EnsureCreated();
+    IUserManager userManager = new UserManager(userContext);
+    return userManager;
 });
 
 var app = builder.Build();
